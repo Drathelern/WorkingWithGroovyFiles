3a7ef1fa70eaa4f87e766e7534eaed7ee03a698d
Drathelern <67207903+Drathelern@users.noreply.github.com>
2021-06-03 10:46:17 -0500

    coma

--- responder/responder.groovy
+++ responder/responder.groovy
@@ -1,9 +1,11 @@
 def hello(greet){ //defining a function called "hello" which receives an argument and stores it in the 
 "greet" variable.
         if (greet == "hi"){ //if the greet value iquals "hello" then executes the println "Hello from responder file!"
-                println 'Hello from responder file!'
-                println 'Hello $NAME'
-        } 
+                println "Hello from responder file!"
+                println "Hello $NAME"
+        } else {
+                println "wzup"
+        }
 }
 
 return this; //returs the output of the execution of the function
