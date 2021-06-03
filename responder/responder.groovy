491c93e18534e1d13eaff897790fbfd1e6556e57
Drathelern <67207903+Drathelern@users.noreply.github.com>
2021-06-03 10:43:25 -0500

    without else

--- responder/responder.groovy
+++ responder/responder.groovy
@@ -3,9 +3,7 @@ def hello(greet){ //defining a function called "hello" which receives an argumen
         if (greet == "hi"){ //if the greet value iquals "hello" then executes the println "Hello from responder file!"
                 println 'Hello from responder file!'
                 println 'Hello $NAME'
-        } else { // if the greet values is diffrent to "hello" then executes the println "wzup"
-                println 'wzup'
-        }
+        } 
 }
 
 return this; //returs the output of the execution of the function
